9167e4cd6f6f66fc08b29258173c137117bd8495
chanchann <www.769440972@qq.com>
2020-12-05 00:13:19 +0800

    esp32 9520

---
+++ esp32_9250_v2/esp32_9250_v2.ino
@@ -0,0 +1,99 @@
+#include <Wire.h>
+#include "I2Cdev.h"
+#include "RTIMUSettings.h"
+#include "RTIMU.h"
+#include "RTFusionRTQF.h" 
+#include "CalLib.h"
+#include <EEPROM.h>
+#include "BluetoothSerial.h"
+
+RTIMU *imu;                                           // the IMU object
+RTFusionRTQF fusion;                                  // the fusion object
+RTIMUSettings settings;                               // the settings object
+BluetoothSerial SerialBT;
+
+//  DISPLAY_INTERVAL sets the rate at which results are displayed
+
+#define DISPLAY_INTERVAL  100                         // interval between pose displays 300 default
+
+//  SERIAL_PORT_SPEED defines the speed to use for the debug serial port
+
+#define  SERIAL_PORT_SPEED  115200
+
+unsigned long lastDisplay;
+unsigned long lastRate;
+int sampleCount;
+
+void setup()
+{
+    int errcode;
+  
+    Serial.begin(SERIAL_PORT_SPEED);
+    Wire.begin();
+    imu = RTIMU::createIMU(&settings);                        // create the imu object
+    SerialBT.begin("ESP32"); 
+
+    Serial.print("ArduinoIMU starting using device "); Serial.println(imu->IMUName());
+    if ((errcode = imu->IMUInit()) < 0) {
+        Serial.print("Failed to init IMU: "); Serial.println(errcode);
+    }
+  
+    if (imu->getCalibrationValid())
+        Serial.println("Using compass calibration");
+    else
+        Serial.println("No valid compass calibration data");
+
+    lastDisplay = lastRate = millis();
+    sampleCount = 0;
+
+    // Slerp power controls the fusion and can be between 0 and 1
+    // 0 means that only gyros are used, 1 means that only accels/compass are used
+    // In-between gives the fusion mix.
+    
+    fusion.setSlerpPower(0.02);
+    
+    // use of sensors in the fusion algorithm can be controlled here
+    // change any of these to false to disable that sensor
+    
+    fusion.setGyroEnable(true);
+    fusion.setAccelEnable(true);
+    fusion.setCompassEnable(true);
+}
+
+void loop()
+{  
+    unsigned long now = millis();
+    unsigned long delta;
+    int loopCount = 1;
+  
+    while (imu->IMURead()) {                                // get the latest data if ready yet
+        // this flushes remaining data in case we are falling behind
+        if (++loopCount >= 10)
+            continue;
+        fusion.newIMUData(imu->getGyro(), imu->getAccel(), imu->getCompass(), imu->getTimestamp());
+        sampleCount++;
+        if ((delta = now - lastRate) >= 1000) {
+            Serial.print("Sample rate: "); Serial.print(sampleCount);
+            if (imu->IMUGyroBiasValid())
+                Serial.println(", gyro bias valid");
+            else
+                Serial.println(", calculating gyro bias");
+        
+            sampleCount = 0;
+            lastRate = now;
+        }
+        if ((now - lastDisplay) >= DISPLAY_INTERVAL) {
+            lastDisplay = now;
+//          RTMath::display("Gyro:", (RTVector3&)imu->getGyro());                // gyro data
+//          RTMath::display("Accel:", (RTVector3&)imu->getAccel());              // accel data
+//          RTMath::display("Mag:", (RTVector3&)imu->getCompass());              // compass data
+//            RTMath::displayRollPitchYaw("Pose:", (RTVector3&)fusion.getFusionPose()); // fused output
+            RTVector3 pose = fusion.getFusionPose();
+//            Serial.println("");
+//            Serial.printf("%6.2f, %6.2f, %6.2f\r\n", pose.x() * RTMATH_RAD_TO_DEGREE, pose.y() * RTMATH_RAD_TO_DEGREE, pose.z() * RTMATH_RAD_TO_DEGREE);
+            SerialBT.printf("%6.2f, %6.2f, %6.2f\r\n", pose.x() * RTMATH_RAD_TO_DEGREE, pose.y() * RTMATH_RAD_TO_DEGREE, pose.z() * RTMATH_RAD_TO_DEGREE);
+//            delay(3000);
+           Serial.println();
+        }
+    }
+}
